c0eab87a71916a3857b31cb3ce5eae648aa01aef
Thanh Vu <vut@lafayette.edu>
2016-11-29 11:22:04 -0500

    Generate model for public_school_enrollment

---
+++ unitedUmbrella/app/models/public_school_enrollment.rb
@@ -0,0 +1,3 @@
+class PublicSchoolEnrollment < ApplicationRecord
+    self.table_name = "public_school_enrollment"
+end
---
+++ unitedUmbrella/test/models/public_school_enrollment_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PublicSchoolEnrollmentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
